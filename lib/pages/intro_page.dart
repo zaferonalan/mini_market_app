e75a01e1bc4896cb5353d9d1a932b0e560bfe890
zafergunayonalan <zafergunay7@gmail.com>
2026-06-19 18:40:10 +0300

    add_introPage

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,74 @@
+// stfull
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mini_market_app/pages/home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // logo
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 80.0,
+              right: 80,
+              bottom: 40,
+              top: 160,
+            ),
+            child: Image.asset('assets/images/deliver.gif'),
+          ),
+
+          // content
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Text(
+              "We deliver groceries at your doorstep",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.notoSerif(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 24),
+          // items
+          Text(
+            "Fresh items everyday",
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+
+          const Spacer(),
+
+          // button
+          GestureDetector(
+            onTap: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const HomePage();
+                },
+              ),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.all(24),
+              child: const Text(
+                "Get Started",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
